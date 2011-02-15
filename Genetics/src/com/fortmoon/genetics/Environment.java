ac351a520c921301bdd4345d504bbceddb6cbb61
Christopher Steel <csteel@syntheticore.com>
2011-02-15 23:38:04 +0000

    Added file header
    git-svn-id: https://alien64/svn/svnrepositoryWD/PrestoKeyGen@144 255e5f77-db3f-6a4b-af53-b4e44ef015aa

--- Genetics/src/com/fortmoon/genetics/Environment.java
+++ Genetics/src/com/fortmoon/genetics/Environment.java
@@ -1,5 +1,28 @@
+/*
+ * @(#)Environment.java $Date: Feb 15, 2011 6:28:27 PM $
+ * 
+ * Copyright © 2011 FortMoon Consulting, Inc. All Rights Reserved.
+ * 
+ * This software is the confidential and proprietary information of FortMoon
+ * Consulting, Inc. ("Confidential Information"). You shall not disclose such
+ * Confidential Information and shall use it only in accordance with the terms
+ * of the license agreement you entered into with FortMoon Consulting.
+ * 
+ * FORTMOON MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE
+ * SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR
+ * NON-INFRINGEMENT. FORTMOON SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY
+ * LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS
+ * DERIVATIVES.
+ * 
+ */
 package com.fortmoon.genetics;
 
+/**
+ * @author Christopher Steel - FortMoon Consulting, Inc.
+ *
+ * @since Feb 15, 2011 6:28:27 PM
+ */
 public interface Environment {
 	
 }
